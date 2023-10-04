b7522f3751955bf378a17406a825b0ec6e0950f1
tecnian <xteixido@tecnian.com>
2023-10-04 11:43:02 +0200

    Update tour.php

--- web/content/tour.php
+++ web/content/tour.php
@@ -3,9 +3,11 @@
         
         <div id="tour360">
             <div class="frame_tour">
-                <iframe id="frameTour" src="https://my.matterport.com/show/?m=sPyzqhGvwBc" frameborder="0" allowfullscreen allow="xr-spatial-tracking" style="width:100%;height:100%"></iframe>
+                <iframe id="frameTour" src="https://my.matterport.com/show/?m=C3x9Lp3QyyK" frameborder="0" allowfullscreen allow="xr-spatial-tracking" style="width:100%;height:100%"></iframe>
             </div>
         </div>
+
+
 
  
         <script>          
